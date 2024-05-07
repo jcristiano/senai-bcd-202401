bcd26578a350b4cdcc4e6caaa701c0b6c18b6ef4
jcristiano <jcristiano.c@gmail.com>
2024-05-07 20:03:24 -0300

    Create crud.py
    Exemplo desenvolvido na aula de 20240506

---
+++ python/exemplo_conexao/crud.py
@@ -0,0 +1,40 @@
+from mysql import connector as MyConnector
+
+def criar_tabela_aluno(conexao):
+    sql = """
+        CREATE TABLE IF NOT EXISTS aluno (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(255),
+            nascimento DATE
+        )
+    """
+    cursor = conexao.cursor()
+    cursor.execute(sql)
+    cursor.close()
+
+def criar_novo_aluno(conexao):
+    nome = input("Digite o nome do aluno: ")
+    nascimento = input("Digite o nascimento no formado dd/mm/YYYY: ")
+    nascimento_mysql = '-'.join(nascimento.split('/')[::-1])
+
+    cursor = conexao.cursor()
+    sql = "INSERT INTO aluno (nome, nascimento) VALUES (%s, %s)"
+    cursor.execute(sql, (nome, nascimento_mysql))
+    conexao.commit()
+    cursor.close()
+
+if __name__ == "__main__":
+    conexao = MyConnector.connect(
+        host='localhost',
+        user='myapp',
+        password='myapp',
+        database='mydbpython',
+        port=3306,
+    )
+
+    if conexao.is_connected:
+        print('Conexao estabelecida')
+
+        criar_tabela_aluno(conexao)
+        criar_novo_aluno(conexao)
+        conexao.close()
